e8e87f24f5189f1492614677ee8af566e14d553f
YulianLyutskanov <126861539+YulianLyutskanov@users.noreply.github.com>
2024-04-02 21:27:06 +0300

    Update main.cpp

--- Practicum/Week6-Group3/NumberSeries/main.cpp
+++ Practicum/Week6-Group3/NumberSeries/main.cpp
@@ -1,6 +1,28 @@
-#include <iostream>
 #include "NumberSeries.h"
+#include <iostream>
 
-int main (){
-  
+int powerSeries(int n)
+{
+    return n + 5;
+}
+
+int nextFibonacci(int n)
+{
+    int a = 0, b = 1;
+    while (true)
+    {
+        int temp = a + b;
+        if (temp > n)
+            return temp;
+        a = b;
+        b = temp;
+    }
+}
+
+int main()
+{
+
+    NumberSeries ns(1, nextFibonacci);
+    ns.getNumberByIndex(50);
+    ns.print();
 }
